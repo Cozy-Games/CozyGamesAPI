22e864abe72808bbc959623874c3340d7a68746d
Smudge <68658429+smuddgge@users.noreply.github.com>
2024-04-19 19:56:14 +0100

    Added aditional classes for maps

--- api/src/main/java/com/github/cozygames/api/item/Item.java
+++ api/src/main/java/com/github/cozygames/api/item/Item.java
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package com.github.cozygames.api.group;
+package com.github.cozygames.api.item;
 
-public class GroupSize {
+public class Item {
 }
