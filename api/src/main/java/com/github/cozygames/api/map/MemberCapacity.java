5e22af7aaca8aae0a885c738e4de2326d41fa16d
Smudge <68658429+smuddgge@users.noreply.github.com>
2024-04-19 22:16:54 +0100

    Added member capacity implementation

--- api/src/main/java/com/github/cozygames/api/map/MemberCapacity.java
+++ api/src/main/java/com/github/cozygames/api/map/MemberCapacity.java
@@ -18,5 +18,101 @@
 
 package com.github.cozygames.api.map;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents the member amounts that can play the map.
+ * <p>
+ * If the amount of members is not listed in this class
+ * they should not be able to play the map.
+ */
 public class MemberCapacity {
+
+    private final @NotNull List<Integer> possibleCapacityList;
+
+    /**
+     * Used to create a new empty member
+     * capacity class.
+     */
+    public MemberCapacity() {
+        this.possibleCapacityList = new ArrayList<>();
+    }
+
+    /**
+     * Used to create a member capacity class.
+     *
+     * @param possibleCapacityList The list of possible capacity's to
+     *                             initialise the list with. This list
+     *                             will be cloned.
+     */
+    public MemberCapacity(@NotNull List<Integer> possibleCapacityList) {
+        this.possibleCapacityList = new ArrayList<>(possibleCapacityList);
+    }
+
+    /**
+     * Get the list of possible member capacity's the map can take.
+     *
+     * @return The list of possible capacity's.
+     */
+    public @NotNull List<Integer> getPossibleCapacityList() {
+        return this.possibleCapacityList;
+    }
+
+    /**
+     * Used to add a possible capacity to the list.
+     *
+     * @param capacity The capacity to add.
+     * @return This instance.
+     */
+    public @NotNull MemberCapacity addPossibleCapacity(int capacity) {
+        this.possibleCapacityList.add(capacity);
+        return this;
+    }
+
+    /**
+     * Used to add a possible capacity list to the
+     * current list of possible capacity's.
+     *
+     * @param possibleCapacityList The possible capacity's to add.
+     * @return This instance.
+     */
+    public @NotNull MemberCapacity addPossibleCapacityList(@NotNull List<Integer> possibleCapacityList) {
+        this.possibleCapacityList.addAll(possibleCapacityList);
+        return this;
+    }
+
+    /**
+     * Used to remove a possible capacity.
+     *
+     * @param capacity The capacity to remove.
+     * @return This instance.
+     */
+    public @NotNull MemberCapacity removePossibleCapacity(int capacity) {
+        this.possibleCapacityList.remove(capacity);
+        return this;
+    }
+
+    /**
+     * Used to remove a collection of possible capacity's.
+     *
+     * @param possibleCapacityList The list of possible capacity's to remove.
+     * @return This instance.
+     */
+    public @NotNull MemberCapacity removePossibleCapacityList(@NotNull List<Integer> possibleCapacityList) {
+        this.possibleCapacityList.removeAll(possibleCapacityList);
+        return this;
+    }
+
+    /**
+     * Used to check if an amount of members is possible.
+     *
+     * @param memberAmount The amount of member's to check.
+     * @return True if the member amount is possible.
+     */
+    public boolean isPossible(int memberAmount) {
+        return this.possibleCapacityList.contains(memberAmount);
+    }
 }
